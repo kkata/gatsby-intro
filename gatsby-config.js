37eaab013d93122daff5136d9844d62e1d2b3228
kkata <14849535+kkata@users.noreply.github.com>
2020-11-05 18:12:41 +0900

    website setup

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Frontend Masters Gatsby Workshop',
+    description: 'brabaraaaa',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+};
